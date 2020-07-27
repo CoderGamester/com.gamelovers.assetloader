ff0809d666c42a1ce516eced4f9f67606dd90157
Miguel Tomas <miguel@firstlight.games>
2020-07-28 00:38:32 +0100

    - Added *AddressablesLoader* to the samples package with the asset loader implementation for the asset loader
    **Changed**
    - Removed the dependency to Unity's Addressables package
    - Removed the Addressables helper methods from *AssetLoaderUtils* to remove any external package dependency
    - Removed the Addressables editor build method
    - Moved *AddressablesIdsGenerator* to the samples package

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,16 @@ All notable changes to this package will be documented in this file.
 The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/)
 and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.html).
 
+## [0.4.0] - 2020-07-26
+
+- Added *AddressablesLoader* to the samples package with the asset loader implementation for the asset loader
+
+**Changed**
+- Removed the dependency to Unity's Addressables package
+- Removed the Addressables helper methods from *AssetLoaderUtils* to remove any external package dependency
+- Removed the Addressables editor build method
+- Moved *AddressablesIdsGenerator* to the samples package
+
 ## [0.3.0] - 2020-07-13
 
 - Added the *IAssetLoader* to allows to wrap the Addressables Loading scheme into an object reference
--- Runtime/IAssetLoader.cs
+++ Runtime/IAssetLoader.cs
@@ -1,13 +1,12 @@
 using System.Threading.Tasks;
 using UnityEngine;
-using UnityEngine.ResourceManagement.ResourceProviders;
 
 // ReSharper disable CheckNamespace
 
 namespace GameLovers.AssetLoader
 {
 	/// <summary>
-	/// This interface allows to wrap the Addressables Loading scheme into an object reference
+	/// This interface allows to wrap the asset loading scheme into an object reference
 	/// </summary>
 	public interface IAssetLoader
 	{
@@ -19,11 +18,21 @@ namespace GameLovers.AssetLoader
 		Task<T> LoadAssetAsync<T>(string path);
 
 		/// <summary>
-		/// Loads and instantiates the prefab in the given <paramref name="path"/> with the given <paramref name="instantiateParameters"/>.
+		/// Loads and instantiates the prefab in the given <paramref name="path"/> with the given <paramref name="parent"/>
+		/// and the given <paramref name="instantiateInWorldSpace"/> to preserve the instance transform relative to world
+		/// space or relative to the parent.
 		/// To help the execution of this method is recommended to request the asset path from an <seealso cref="AddressableConfig"/>.
 		/// This method can be controlled in an async method and returns the prefab instantiated
 		/// </summary>
-		Task<GameObject> InstantiatePrefabAsync(string path, InstantiationParameters instantiateParameters = new InstantiationParameters());
+		Task<GameObject> InstantiatePrefabAsync(string path, Transform parent, bool instantiateInWorldSpace);
+
+		/// <summary>
+		/// Loads and instantiates the prefab in the given <paramref name="path"/> with the given <paramref name="position"/>,
+		/// the given <paramref name="rotation"/> & the given <paramref name="parent"/>.
+		/// To help the execution of this method is recommended to request the asset path from an <seealso cref="AddressableConfig"/>.
+		/// This method can be controlled in an async method and returns the prefab instantiated
+		/// </summary>
+		Task<GameObject> InstantiatePrefabAsync(string path, Vector3 position, Quaternion rotation, Transform parent);
 
 		/// <summary>
 		/// Unloads the given <paramref name="asset"/> from the game memory.
